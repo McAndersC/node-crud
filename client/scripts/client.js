55c44d63c8f7157e539337a7d76da29d59a74711
Anders Christensen <anders@medieskolerne.dk>
2023-09-04 15:39:48 +0200

    Added users

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+const client = {};
+
+client.userTmpl = (user) => {
+
+    return `<h1>HEJ MED DIG ${user.name} - ${user.age}</h1>`
+
+}
+
+
+client.init = () => {
+
+    const userList = document.querySelector('#user-list');
+
+    fetch('http://localhost:3000/users').then(res => res.json()).then(data => {
+
+      
+        userList.innerHTML = "";
+        data.map( (user) => {
+
+            userList.insertAdjacentHTML("beforeend", client.userTmpl(user))
+
+        } )
+
+    
+    });
+
+};
+
+
+client.init();
+
+
